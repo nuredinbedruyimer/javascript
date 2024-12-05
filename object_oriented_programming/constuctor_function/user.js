c7e8de5d5bca3b26de090817b7050d3fec28683a
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-06 00:48:15 +0300

    Feat: Convert 'constructor Function' To 'ES2015 Class'

--- object_oriented_programming/constuctor_function/user.js
+++ object_oriented_programming/constuctor_function/user.js
@@ -1,12 +1,14 @@
-let Person = function (name, gender, yearOfBirth) {
-  this.name = name;
-  this.gender = gender;
-  this.yearOfBirth = yearOfBirth;
-  this.getAge = function () {
-    const age = new Date().getFullYear() - this.yearOfBirth;
-    console.log("Age: ", age);
-  };
-};
+class Person {
+  constructor(name, gender, yearOfBirth) {
+    this.name = name;
+    this.gender = gender;
+    this.yearOfBirth = yearOfBirth;
+    this.getAge = function () {
+      const age = new Date().getFullYear() - this.yearOfBirth;
+      console.log("Age: ", age);
+    };
+  }
+}
 
 const userOne = new Person("Nuredin Bedru", "Male", 2001);
 userOne.getAge();
