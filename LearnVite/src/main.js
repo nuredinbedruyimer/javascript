1a9096477385e4f2698cf4e6b53bc91c9dc2c348
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-05 14:19:16 +0300

    feat : Learn Env Variable In Detail  In Vite Project

--- LearnVite/src/main.js
+++ LearnVite/src/main.js
@@ -1,16 +1,1 @@
-import imageUrl from "./assets/images/javascript.svg";
-import { name, favCharacter, title } from "nuredin/testJson.json";
-
-const node = document.createElement("img");
-const elementForName = document.createElement("p");
-const text1 = document.createTextNode(name);
-
-elementForName.appendChild(text1);
-
-node.setAttribute("src", imageUrl);
-node.setAttribute("alt", "Image AlternaiveName");
-
-document.getElementById("app").appendChild(node);
-document.getElementById("app").appendChild(elementForName);
-
-console.log(data);
+console.log("Enveromental Variables : ", import.meta.env);
--- LearnVite/vite.config.js
+++ LearnVite/vite.config.js
@@ -8,6 +8,7 @@ export default {
       nuredin: path.resolve(__dirname, "./src/json"),
     },
   },
+  envPrefix: "NUREDIN",
   publicDir: "public",
   plugins: [Inspect()],
   esbuild: {
