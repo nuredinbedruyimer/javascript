9f81b8904f735fdaccd61ac5a98e6b59b3a9bbaa
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-07 02:15:27 +0300

    Feat:'Refactoring'

--- object_oriented_programming/modules/index.js
+++ object_oriented_programming/modules/index.js
@@ -1,15 +1,3 @@
 const arthimetics = await import("./arthimetic.js");
 
 const modules = await import("./greet.js");
-
-//  Arthimetics
-
-console.log("Addition From Dynamic Import : ", arthimetics.add(12, 13));
-console.log("Subtraction From Dynamic Import : ", arthimetics.sub(12, 13));
-console.log("Division From Dynamic Import : ", arthimetics.div(12, 13));
-console.log("Multiplication From Dynamic Import : ", arthimetics.mul(12, 13));
-
-//  Greetings
-
-console.log(modules.sayHey("Nuredin Bedru"));
-console.log(modules.default("Nuredin Bedru"));
