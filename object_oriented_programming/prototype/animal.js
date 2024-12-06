657d7295d761be55e7fd91d096ee16fc7be7813e
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-07 00:38:55 +0300

    Fix:Change The Constructor Function With 'ES2015 Class'

--- object_oriented_programming/prototype/animal.js
+++ object_oriented_programming/prototype/animal.js
@@ -1,23 +1,25 @@
-function Animal(species) {
-  this.species = species;
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
+  talk() {
+    return `I AM Able To Talk And my species Is ${this.species}`;
+  }
+  eat() {
+    return `I AM Able To Eat And my species Is ${this.species}`;
+  }
 }
-Animal.prototype.talk = function () {
-  return `I AM Able To Talk And my species Is ${this.species}`;
-};
-Animal.prototype.eat = function () {
-  return `I AM Able To Eat And my species Is ${this.species}`;
-};
 
-function Dog(ownerName) {
-  Animal.call(this, "Dog");
-  this.ownerName = ownerName;
+class Dog extends Animal {
+  constructor(species, ownerName) {
+    super(species);
+    this.ownerName = ownerName;
+  }
+  bark() {
+    return `Wooof Woooof Because I Am '${this.species} and Owned By '${this.ownerName}'`;
+  }
 }
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog;
-Dog.prototype.bark = function () {
-  return `Wooof Woooof Because I Am '${this.species} and Owned By '${this.ownerName}'`;
-};
 
-const dogOne = new Dog("Nuredin Bedru");
+const dogOne = new Dog("Dog", "Nuredin Bedru");
 
 console.log("Dog Object : ", dogOne.bark());
