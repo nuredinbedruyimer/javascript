8c7d24c08b60548330a27bae88e337abe5e4399f
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-07 01:19:00 +0300

    Feat: Export And Import Module Just Using 'Export' Not Default

---
+++ object_oriented_programming/modules/index.js
@@ -0,0 +1,8 @@
+import { sub, add, mul, div } from "./arthimetic.js";
+
+console.log("Addition : ", add(12, 13));
+console.log("Subtraction : ", sub(32, 12));
+console.log("Multiplication : ", mul(32, 12));
+console.log("Division : ", div(32, 12));
+
+export default {};
